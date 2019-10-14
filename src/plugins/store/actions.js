f445caf9ac14d2abef164dd7e033b1bb0abd4f92
Vladislav Ladicky <vladislav.ladicky@lektor.itlearning.sk>
2019-10-14 11:56:25 +0200

    hardwired api url moved to env file

--- src/plugins/store/actions.js
+++ src/plugins/store/actions.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  // TODO: move this hardwired url to .env file
-  baseURL: 'http://localhost:3000'
+  baseURL: process.env.VUE_APP_API_BASE
 })
 
 export default {
